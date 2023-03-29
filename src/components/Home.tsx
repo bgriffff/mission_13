ef3eab40dee7a9b8431dde7a0437bf052bd5cfb5
bgriffff <brandonjosephgriffiths@gmail.com>
2023-03-28 21:49:29 -0600

    Initial

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import joel from "../img/JoelHiltonHeadshot.jpg";
+
+export const Home = () => {
+  return (
+    <div className="App-header">
+      <h1>Welcome to my Website!</h1>
+      <br></br>
+
+      <img src={joel} alt="Joel" />
+    </div>
+  );
+};
